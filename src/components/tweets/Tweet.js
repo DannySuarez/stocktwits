1b6ff4711cb28551ca2acd99349fbcdd2daca4b0
DannySuarez <suarezd10@gmail.com>
2019-12-18 12:30:11 -0800

    Add materil ui components

--- src/components/tweets/Tweet.js
+++ src/components/tweets/Tweet.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import { List, ListItem, Divider, ListItemText, Typography } from '@material-ui/core';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: '100%',
+    backgroundColor: theme.palette.background.paper
+  },
+  inline: {
+    display: 'inline'
+  }
+}));
 
 function Tweet({ tweet }) {
+  const classes = useStyles();
+
   return (
-    <li>
-      {tweet.body}
-    </li>
+    <List className={classes.root}>
+      <ListItem alignItems="flex-start">
+        <ListItemText
+          secondary={
+            <>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                {tweet.user.name} 
+              </Typography>
+              -- {tweet.body}
+            </>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li"/>
+
+    </List>
   );
 }
 
