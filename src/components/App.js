c52d6b58f616c95c3793aed68ec71595b3c1178c
DannySuarez <suarezd10@gmail.com>
2019-12-20 11:12:18 -0800

    Add Navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 import Search from './containers/Search';
+import WatchList from './containers/WatchList';
 
 export default function App() {
   return (
-    <Search />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Search} />
+        <Route path="/watchlist" component={WatchList} />
+      </Switch>
+    </Router>
   );
 
   
